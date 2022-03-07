b6b47c1b2516cad59bd2261432d2ea2bcd58787b
Silvestr <40587282+silvestr-chaiko@users.noreply.github.com>
2022-03-07 11:12:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
+    
+    
+    let i = 0;
+    let result = '';
+    
+    while (i < n.length) {
+    
+    result = n[i] + result;
+    i = i + 1;
+    
+    }
+      
+    return result;   
   
 }
